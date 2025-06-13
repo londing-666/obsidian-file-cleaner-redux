d5a290f71e292e25022b798c8ef15a5c7879af85
londing-666 <64513245+londing-666@users.noreply.github.com>
2025-06-13 16:51:15 +0800

    Update zh-cn.ts

--- src/locales/zh-cn.ts
+++ src/locales/zh-cn.ts
@@ -2,41 +2,154 @@ import { Locale } from "./locale";
 import enUS from "./en";
 
 //Simplified Chinese - 简体中文
-export const zhCN: Locale = {
+const zhCN: Locale = {
   Settings: {
     RegularOptions: {
       CleanedFiles: {
-        Label: "清理",
-        Description: "要如何处理已清理的文件？",
+        Label: "清理文件",
+        Description: "What happens to a file after it's deleted.",
 
         Options: {
-          MoveToSystemTrash: "移至系统回收站",
-          MoveToObsidianTrash: "移至软件回收站（.trash 文件夹）",
-          PermanentDelete: "永久删除",
+          MoveToSystemTrash: "Move to system trash",
+          MoveToObsidianTrash: "Move to Obsidian trash (.trash folder)",
+          PermanentDelete: "Permanently delete",
         },
       },
+      ObsidianTrashCleanupAge: {
+        Label: "Trash directory age threshold",
+        Description:
+          "Amount of days files can be in the `.trash` folder before they are permanently removed (minimum 1 day).",
+      },
 
-      ExcludedFolders: {
-        Label: "排除文件夹",
+      FolderFiltering: {
+        Excluded: {
+          Label: "Excluded folders",
+          Description: `
+          Folders that should be excluded during cleanup, all other folders will be scanned.
+          Paths are case-sensitive.
+          One folder per line.
+          Supports regular expressions (wildcard matching can be done using \`.*\`)`,
+        },
+        Included: {
+          Label: "Included folders",
+          Description: `
+          Folders that should be included during cleanup, only these folders will be scanned.
+          Paths are case-sensitive.
+          One folder per line.
+          Supports regular expressions (wildcard matching can be done using \`.*\`)`,
+        },
+        Label: "Excluded / Included Folders",
+        Placeholder: "Example:\nfolder/subfolder\nfolder2/subfolder2",
+        Description:
+          "The folders below should be excluded from or included in the cleanup process.",
+      },
+
+      Attachments: {
+        Excluded: {
+          Label: "Excluded Attachment extensions",
+          Description:
+            "List of extensions that should be ignored during cleanup, all other files are included, the `.*` wildcard can be used to select all extensions. Comma-separated.",
+          Placeholder: "Example:.jpg, .png, .pdf, .*",
+        },
+        Included: {
+          Label: "Included Attachment extensions",
+          Description:
+            "List of extensions that should be included during cleanup, all other files are ignored, the `.*` wildcard can be used to select all extensions. Comma-separated.",
+          Placeholder: "Example:.jpg, .png, .pdf, .*",
+        },
+        Label: "Excluded / Included Extensions",
+        Description:
+          "The attachment extensions below should be excluded from or included in the cleanup process.",
+      },
+
+      IgnoredFrontmatter: {
+        Label: "Ignored frontmatter",
         Description: `
-          排除文件夹中的文件将不会被清理。
-          路径区分大小写，每个路径由换行符分隔。`,
-        Placeholder: "示例：\n文件夹/子文件夹\n文件夹2/子文件夹2",
+          List of frontmatter properties that should be ignored during cleanup.
+          If a file contains only frontmatter and contains only these properties, the file will be removed, comma-separated.
+        `,
+        Placeholder: "Example:\ncreated, updated",
+      },
+
+      IgnoreAllFrontmatter: {
+        Label: "Ignore all frontmatter",
+        Description: "Ignores all frontmatter, including the ones set above.",
+      },
+
+      CodeblockParsing: {
+        Label: "Codeblock parsing",
+        Description: `
+          List of codeblock-types that should be checked for attachments.
+          Supports regular expressions.
+          Comma-separated.
+        `,
+        Placeholder: "Example:\nad-summary, ad-.*, tabs",
+      },
+
+      FileAgeThreshold: {
+        Label: "File age threshold",
+        Description:
+          "Only files and folders over the specified threshold will be cleaned up (in number of days)",
+      },
+
+      PreviewDeletedFiles: {
+        Label: "Preview deleted files",
+        Description: "Show a confirmation box with list of files to be removed",
+      },
+
+      DeleteEmptyMarkdownFiles: {
+        Label: "Delete empty Markdown files",
+        Description: "Removes Markdown files if their size is 0",
+      },
+
+      RemoveFolders: {
+        Label: "Remove folders",
+        Description: "Include folders in cleanup",
+      },
+
+      RunOnStartup: {
+        Label: "Run on startup",
+        Description: "Runs the cleaner on startup",
+      },
+    },
+
+    DangerZone: {
+      Header: "Danger zone",
+
+      ResetSettings: {
+        Label: "Reset settings",
+        Description: "Resets the configuration back to default values",
+        Button: "Reset",
       },
     },
   },
 
+  Modals: {
+    ResetSettings: {
+      Title: "Reset settings",
+      Text: "Are you sure you want to reset the settings?",
+    },
+    DeletionConfirmation: {
+      Title: "Deletion confirmation",
+      Text: "The following will be deleted",
+
+      Files: "Files",
+      Folders: "Folders",
+    },
+
+    ButtonCancel: "Cancel",
+    ButtonConfirm: "Confirm",
+  },
+
   Buttons: {
-    CleanFiles: "清理文件",
+    CleanFiles: "Clean files",
   },
 
   Notifications: {
-    CleanSuccessful: "清理成功",
-    NoFileToClean: "没有文件需要清理",
+    CleanSuccessful: "Clean successful",
+    NoFileToClean: "No file to clean",
+    SettingsReset: "File Cleaner Redux: Setting reset",
   },
-
-  // The following is used as a fallback in case a translation is missing.
-  ...enUS,
 };
 
 export default zhCN;
